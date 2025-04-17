933ef92473ec334cb1dbba3b75f66c9957877dbd
SkyLabs-dev <sl.rio.mobile7@gmail.com>
2025-04-17 15:35:08 +0900

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -25,9 +25,9 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.SkyLabs-dev"
             artifactId = "cart"
-            version = "1.0.2"
+            version = "1.0.3"
             // AAR 파일 경로
-            artifact(file("$projectDir/cartsdk-release.aar"))
+            artifact(file("$projectDir/cartsdk-release-1.0.3.aar"))
         }
     }
 }
