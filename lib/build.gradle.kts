0d45fcd93f720771a02ec5f6a8160eb96bbcfc40
jonghyuk.park <jonghyuk.park@skylabs.io>
2025-07-23 08:24:42 +0900

    JitPack 빌드 오류 수정 (Toolchain)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -24,7 +24,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.SkyLabs-dev"
             artifactId = "cart"
-            version = "1.1.59"
+            version = "1.1.60"
             artifact(file("$projectDir/cartsdk-release.aar"))
         }
     }
