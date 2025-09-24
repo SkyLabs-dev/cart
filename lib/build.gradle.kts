62da88f6772043c74f9687c505d110ac7dcaa061
jonghyuk.park <jonghyuk.park@skylabs.io>
2025-09-24 18:10:09 +0900

    Update 1.1.120

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -24,7 +24,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.SkyLabs-dev"
             artifactId = "cart"
-            version = "1.1.119"
+            version = "1.1.120"
             artifact(file("$projectDir/cartsdk-release.aar"))
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,5 +11,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
-rootProject.name = "cart-sdk"
+rootProject.name = "CART_SDK_JIT"
 include("lib")
