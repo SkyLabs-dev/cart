6f7b4e49174835eee92d7be3e847aa40e9e06f04
jonghyuk.park <jonghyuk.park@skylabs.io>
2025-12-26 10:37:32 +0900

    1.2.69

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -24,7 +24,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.SkyLabs-dev"
             artifactId = "cart"
-            version = "1.2.68"
+            version = "1.2.69"
             artifact(file("$projectDir/cartsdk-release.aar"))
         }
     }
