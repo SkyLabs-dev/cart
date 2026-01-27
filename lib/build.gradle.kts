8c6abf0b778a52646be3ae63ff2a7515ca99ec70
jonghyuk.park <jonghyuk.park@skylabs.io>
2026-01-27 10:41:14 +0900

    Update 1.2.89

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -24,7 +24,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.SkyLabs-dev"
             artifactId = "cart"
-            version = "1.2.88"
+            version = "1.2.89"
             artifact(file("$projectDir/cartsdk-release.aar"))
         }
     }
